21e93269f90a9b1b183ceb601f882a011174110b
Hardeep Sinha <hardeepsinha21@gmail.com>
2022-08-01 11:13:48 +0530

    Added remaining targets
    Added remaining targets screen
    retry button has been added and logic is yet to be written

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ window.onload = function(){
     majorText = $("#majorText");
     secondaryText = $("#secondaryText");
     scoreText = $("#scoreText");
+    RemainingTargets = $("#RemainingTargets");
+    retryBtn = $('#retryBtn');
     var minLeftPos = 10;
-    var minTopPos = 55;
+    var minTopPos = 120;
     var maxLeftPos = TestScreen.width() - 125;
-    var maxTopPos = TestScreen.height() + 15;
+    var maxTopPos = TestScreen.height() + 65 - 50;
     var isFirstClick = true;
     var totalTime = 0;
     var prevTime = 0;
@@ -20,22 +22,26 @@ window.onload = function(){
             majorText.text("Game Over!!!")
             target.css({position:'inherit'});
             secondaryText.text("Average time per target");
+            RemainingTargets.text('');
             scoreText.text(Math.round((totalTime-10)/20) + " ms");
             return;
         }
         var leftPos = minLeftPos + Math.random()*(maxLeftPos-minLeftPos);//min 10px and max 
-        var topPos = minTopPos + Math.random()*(maxTopPos-minTopPos);//min 55px and max
+        var topPos = minTopPos + Math.random()*(maxTopPos-minTopPos);//min 120px and max
         if(isFirstClick){
             majorText.text("");
             secondaryText.text("");
+            RemainingTargets.text(`Remaining ${remainingClicks}`);
+            retryBtn.hide();
             isFirstClick = false;
-            target.css({position:'absolute', left: leftPos, top: topPos})
+            target.css({position:'absolute', left: leftPos, top: topPos});
             prevTime = Date.now();
             remainingClicks--;
             return;
         }
         // leftPos = 10 + TestScreen.width()- 135; 
         // topPos = 55 + TestScreen.height() - 40;
+        RemainingTargets.text(`Remaining ${remainingClicks}`);
         totalTime += Date.now() - prevTime;
         target.css({left: leftPos, top: topPos})
         prevTime = Date.now();
